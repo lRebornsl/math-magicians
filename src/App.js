76b1623d40c3f5ad020acaf49394cdc97b662939
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-04 18:18:19 -0500

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// eslint-disable-next-line
 import Calculator from './components/Calculator';
 
 const App = () => (
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line
 import RenderCalculator from './functions/RenderCalculator';
 
 const Calculator = () => (
