71e0a60a553f419ce88a26ce6bdff8225cf9bd7b
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-05 16:25:08 -0500

    Add events to calculator

--- src/components/functions/renderCalculator.js
+++ src/components/functions/renderCalculator.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
-const RenderCalculator = () => {
-  const calcData = ['AC', '+/-', '%', '/', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+const RenderCalculator = ({ data, setData }) => {
+  const calcData = ['AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '='];
+
+  const updateData = (item) => {
+    const updateObj = calculate(data, item);
+    setData(updateObj);
+  };
 
   return (
     calcData.map((item) => (
-      <div key={item} className={`calcItem ${item === '0' ? 'itemZero' : ''} ${item === '/' || item === 'x' || item === '-' || item === '+' || item === '=' ? 'itemOperation' : ''}`}>
-        <p>{item}</p>
+      <div key={item} className={`calcItem ${item === '0' ? 'itemZero' : ''} ${item === '÷' || item === 'x' || item === '-' || item === '+' || item === '=' ? 'itemOperation' : ''}`}>
+        <button type="button" onClick={() => updateData(item)}>
+          {item}
+        </button>
       </div>
     ))
   );
