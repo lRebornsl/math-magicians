cf39eb9167e89ea848e7e2b4acf7c36128eb196f
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-06 17:34:15 -0500

    Add a quote to the page from an API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Quote from './components/functions/quotes';
 
 const App = () => (
   <div className="App">
     <Calculator />
+    <Quote />
   </div>
 );
 
