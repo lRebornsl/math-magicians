815cc5d9b31f70e532c726913804e4d81b54c549
lRebornsl <anthony_vasquez98@hotmail.com>
2023-07-06 20:28:15 -0500

    Add error state

--- src/components/functions/quotes.js
+++ src/components/functions/quotes.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const Quote = () => {
   const [data, setData] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [error, setError] = useState(false);
   const url = 'https://api.api-ninjas.com/v1/quotes?category=courage';
 
   useEffect(() => {
@@ -12,12 +13,14 @@ const Quote = () => {
         headers: { 'X-Api-Key': '0/oKKV8YUUh7h5bLfAzX0g==AiN9WmbJgj4beaWU' },
       })
         .catch((error) => {
+          setError(true);
           if (error.response) {
             const errorArray = { data: [{ quote: 'We learn from mistakes', author: 'lRebornsl', category: '' }] };
             return errorArray;
           }
           return null;
         });
+
       setData(quote.data[0]);
     };
 
@@ -26,6 +29,14 @@ const Quote = () => {
       getData();
     }
   }, [loaded]);
+
+  if (error) {
+    return (
+      <div className="quote">
+        <h3>We learn from mistakes -lRebornsl</h3>
+      </div>
+    );
+  }
 
   return (
     <div className="quote">
