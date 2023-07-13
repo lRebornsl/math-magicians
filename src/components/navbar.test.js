c4cbc03878e5efe7339e7338b6a14d6e6da8c71d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 12:16:32 -0600

    create navbar.test.js file, import Testing Libraries

---
+++ src/components/navbar.test.js
@@ -0,0 +1,7 @@
+// import Testing Libraries
+import "@testing-library/jest-dom"; 
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import userEvent from '@testing-library/user-event'
+
+
