1b25041f52fa89e27e4fd6f44ceb28fbbeff92b9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 11:57:46 -0600

    Implement Test for Home Componenet

---
+++ src/components/home.test.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom"; 
+import { render, screen } from "@testing-library/react";
+import Home from "./home"
+
+describe('Test Home', () => {
+    test('Home should Render "Welcome to our page!"', () => {
+        render(
+            <Home />
+        )
+    
+        expect(screen.getByText("Welcome to our page!")).toBeInTheDocument()
+    })
+})
