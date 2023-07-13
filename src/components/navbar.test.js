e01f5c92bc453f02073ac17d3cbf8b785ceb55ff
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-07-13 12:17:10 -0600

    import components to be tested

--- src/components/navbar.test.js
+++ src/components/navbar.test.js
@@ -4,4 +4,11 @@ import { render, screen } from "@testing-library/react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import userEvent from '@testing-library/user-event'
 
+// import components to be tested
+
+import Navbar from "./navbar"
+import Calculator from "./calculator"
+import Home from "./home"
+import Quote from "./functions/quotes"
+
 
